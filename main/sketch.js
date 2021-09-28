1739daeb0a6a3b7e4fc1caa205fa03830a98a425
jordyn-Steph <remmysmail@gmail.com>
2021-09-28 12:10:17 -0600

    starting with classes (main proj)

--- main/sketch.js
+++ main/sketch.js
@@ -1,6 +1,7 @@
 //interactive scene with base controls and shapes for the major project. extra for experts is my own shape, sounds will be in a later part of the project.
 
 // global variables
+let Bullet;
 let gameover = true;
 let x = 300;
 let y = 450;
@@ -10,16 +11,35 @@ let speed = 4;
 let backgroundColor = "black";
 let ball;
 //just makes the canvas 
+class bullet {
+  constructor (){
+    this.x = 300;
+    this.y = 100;
+  }
+  function show(){
+    stroke(255);
+    noFill();
+    rect(this.x,this.y,10,10);
+  }
+  function moveAndHitDet() {
+    bullet.y += 1;
+    let hit = collideRectRect(this.x,this.y,10,10,x,y,80,5);
+    if (hit) {
+      console.log("hit");
+    }
+}
+}
 function setup() {
   createCanvas(700,500);
+  Bullet = new bullet();
 }
 //loop to make the background black (might change in the future, hence the variable), display the player and what im going to call "shape" for now (displayEntities), and handle the controls (handleKeys)
 function draw() {
   background (backgroundColor);
   displayEntities();
   handleKeys();
-  fire();
-  y2 += 1;
+  bullet.moveAndHitDet();
+  bullet.show();
 }
 function handleKeys() {
   if (mouseIsPressed) {
@@ -50,10 +70,4 @@ function displayEntities() {
   vertex(420, 60);
   endShape(CLOSE);
 }
-function fire() {
-  rect (x2,y2,10,10);
-  hit = collideRectRect(x2,y2,10,10,x,y,80,5);
-  if (hit) {
-    console.log("hit");
-  }
 }
